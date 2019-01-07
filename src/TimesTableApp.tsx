abac128e3aa4105045d4145e6eda0ed9cbcd952f
Andrew Crites <acrites@mobiquityinc.com>
2019-01-07 08:53:43 -0500

    feat(upgrading): use color-convert for vibrant color hsl

--- src/TimesTableApp.tsx
+++ src/TimesTableApp.tsx
@@ -1,43 +1,11 @@
 import * as React from 'react';
-
 import styled from '@emotion/styled';
+import { random } from 'lodash';
+import * as convert from 'color-convert';
 
 import { TimesTable } from './visualization/TimesTable';
 import { TimesTableControls } from './TimesTableControls';
 import { About } from './About';
-
-import { random } from 'lodash';
-
-//tslint:disable
-function hslToHex(h, s, l) {
-  h /= 360;
-  s /= 100;
-  l /= 100;
-  let r, g, b;
-  if (s === 0) {
-    r = g = b = l; // achromatic
-  } else {
-    const hue2rgb = (p, q, t) => {
-      if (t < 0) t += 1;
-      if (t > 1) t -= 1;
-      if (t < 1 / 6) return p + (q - p) * 6 * t;
-      if (t < 1 / 2) return q;
-      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-      return p;
-    };
-    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    const p = 2 * l - q;
-    r = hue2rgb(p, q, h + 1 / 3);
-    g = hue2rgb(p, q, h);
-    b = hue2rgb(p, q, h - 1 / 3);
-  }
-  const toHex = x => {
-    const hex = Math.round(x * 255).toString(16);
-    return hex.length === 1 ? '0' + hex : hex;
-  };
-  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
-}
-// tslint:enable
 
 const ForkImage = styled.a`
   position: fixed;
@@ -73,7 +41,9 @@ export class TimesTableApp extends React.Component {
   };
 
   getVibrantColor() {
-    return hslToHex(random(0, 360), random(70, 100), random(35, 55));
+    return (
+      '#' + convert.hsl.hex(random(0, 360), random(70, 100), random(35, 55))
+    );
   }
 
   play = () => {
